186ebdb2bf2639742d01b0d7a7bfe37179cb4d2d
urielch <uriel.chemouni@gmail.com>
2023-06-02 20:10:43 +0300

    improve cmake + fill binding

--- binding.cpp
+++ binding.cpp
@@ -1,8 +1,54 @@
 #include <napi.h>
-#include "llama/llama.h" // include the header file of the C++ code you want to bind
+#include "llama/llama.h"
+
+class LlamaContext : public Napi::ObjectWrap<LlamaContext> {
+public:
+    static Napi::Object Init(Napi::Env env, Napi::Object exports);
+    LlamaContext(const Napi::CallbackInfo& info);
+    ~LlamaContext();
+
+private:
+    static Napi::FunctionReference constructor;
+    struct llama_context* _llamaCtx;
+
+    // Binding methods
+    void Free(const Napi::CallbackInfo& info);
+};
+
+Napi::Object LlamaContext::Init(Napi::Env env, Napi::Object exports) {
+    Napi::HandleScope scope(env);
+
+    Napi::Function func = DefineClass(env, "LlamaContext", {
+        InstanceMethod("free", &LlamaContext::Free)
+    });
+
+    constructor = Napi::Persistent(func);
+    constructor.SuppressDestruct();
+
+    exports.Set("LlamaContext", func);
+    return exports;
+}
+
+LlamaContext::LlamaContext(const Napi::CallbackInfo& info) : Napi::ObjectWrap<LlamaContext>(info)  {
+    _llamaCtx = nullptr;
+}
+
+LlamaContext::~LlamaContext() {
+    if (_llamaCtx != nullptr) {
+        llama_free(_llamaCtx);
+    }
+}
+
+void LlamaContext::Free(const Napi::CallbackInfo& info) {
+    if (_llamaCtx != nullptr) {
+        llama_free(_llamaCtx);
+        _llamaCtx = nullptr;
+    }
+}
 
 Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-    // add your binding code here
+    llama_init_backend();
+    LlamaContext::Init(env, exports);
     return exports;
 }
 
